582853a9618bf5884b6db0c6a8d9d1a476178cb3
Jetro de Leon <j08mark@hotmail.com>
2023-09-20 13:52:29 -0500

    Filled MyGameMode cpp file

--- Source/COMP5015A01/Game/MyGameMode.cpp
+++ Source/COMP5015A01/Game/MyGameMode.cpp
@@ -2,9 +2,39 @@
 
 
 #include "MyGameMode.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 
 void AMyGameMode::BeginPlay() {
 
+	if (DefaultScoreWidget) {
+		ScoreWidget = CreateWidget<UUserWidget>(GetWorld(), DefaultScoreWidget);
+		ScoreWidget->AddToViewport();
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *FString("DefaultScoreWidget has not been set."));
+	}
+
+	OnUpdateScore.Broadcast(CurrentScore);
+}
+
+void AMyGameMode::GameCompleted(bool PlayerWon) {
+	if (DefaultGameCompleteWidget) {
+		GameCompleteWidget = CreateWidget<UUserWidget>(GetWorld(), DefaultGameCompleteWidget);
+		if (GameCompleteWidget) {
+			GameCompleteWidget->AddToViewport();
+			UTextBlock* LostOrComplete = Cast<UTextBlock>(
+				GameCompleteWidget->GetWidgetFromName(TEXT("LostOrComplete"))
+			);
+			if (LostOrComplete) {
+				const FText WinLossMessage = PlayerWon ? FText::FromString("You Won!") : FText::FromString("You Lost!");
+				LostOrComplete->SetText(WinLossMessage);
+			}
+		}
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *FString("DefaultGameCompleteWidget has not been set."));
+	}
 }
 
 void AMyGameMode::UpdateScore_Implementation(float DeltaScore) {
